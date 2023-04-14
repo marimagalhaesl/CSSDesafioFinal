82a4402238241559dba3609d01b2e9a0d8e1f056
Mariana Magalhães <marianamagalhaes@Marianas-MacBook-Pro.local>
2023-04-14 15:39:27 -0300

    update home com js

---
+++ src/function.js
@@ -0,0 +1,9 @@
+function changeTheme() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
+ }
+
+ function alertaRegistro(){
+    alert("Você está registrado! Xoxo Gossip Soul");
+    document.getElementById("myForm").reset();
+ }
